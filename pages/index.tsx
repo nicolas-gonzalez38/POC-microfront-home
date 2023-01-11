6e4693cd6f09354f37e0041c09f38edf34553f9b
Nicolas Gonzalez <ngonzalez@loyalty.com>
2023-01-11 13:34:27 -0300

    feat: :tada: pass to next 12

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -15,49 +12,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+        
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
+        
 
         <div className={styles.grid}>
           <a
@@ -66,10 +23,10 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={inter.className}>
+            <h2 >
               Docs <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
+            <p >
               Find in-depth information about Next.js features and&nbsp;API.
             </p>
           </a>
@@ -80,10 +37,10 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={inter.className}>
+            <h2 >
               Learn <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
+            <p >
               Learn about Next.js in an interactive course with&nbsp;quizzes!
             </p>
           </a>
@@ -94,10 +51,10 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={inter.className}>
+            <h2 >
               Templates <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
+            <p >
               Discover and deploy boilerplate example Next.js&nbsp;projects.
             </p>
           </a>
@@ -108,10 +65,10 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={inter.className}>
+            <h2 >
               Deploy <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
+            <p >
               Instantly deploy your Next.js site to a shareable URL
               with&nbsp;Vercel.
             </p>
